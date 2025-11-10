659a418aa6498fb460d083df7bf89cc43ffc08ce
Sayan <officialsayan36@gmail.com>
2025-11-10 12:51:48 +0000

    add an border around the profile icon

--- src/components/layout/user-nav.tsx
+++ src/components/layout/user-nav.tsx
@@ -31,7 +31,7 @@ export function UserNav() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-9 w-9">
+          <Avatar className="h-9 w-9 border-2 border-primary/50">
             <AvatarImage
               src={user?.photoURL ?? ""}
               alt="User avatar"
